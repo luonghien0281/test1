320d6f8c3bf4c9f1e59491069f740d467fd78e4f
르엉탄히엔 <93697965+luonghien0281@users.noreply.github.com>
2022-06-17 01:11:52 +0900

    delete one file and creat one file then change him

---
+++ ThuNghiem1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"toi la hien pro\n hom nay toi thu cai git bash chay terminal trong vscode\n da thanh cong\n va tao branch code o trong local repository\n";
+    return 0;
+}
